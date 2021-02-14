74385d01e1683fa846ac5507f8f878e897150bf8
Jai Kumar <jai.kumar@splashmath.com>
2021-02-14 22:38:38 +0530

    basic clean up, and added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMfgP5H9gEjgoPVht939LHXmW48eLdebI",
+  authDomain: "i-comm.firebaseapp.com",
+  projectId: "i-comm",
+  storageBucket: "i-comm.appspot.com",
+  messagingSenderId: "577326526289",
+  appId: "1:577326526289:web:59b9a7eba8b6faba5cad70",
+  measurementId: "G-SM62V22N4Q"
+};

126eb88ffbe34f2d1a1b97a2e9d356b6806371ad
Jai Kumar <jai.kumar@splashmath.com>
2021-02-15 00:04:38 +0530

    added product component, incomplete css

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Home.css';
+import Product from './Product';
 
 function Home() {
   return (
-    <div>
-      <p>Home Comp</p>
+    <div className="home">
+      <div className="home__container">
+        <img 
+          src="https://www.yayindia.com/sites/default/files/styles/featured/public/post/images/2020/07/17/amazon_slashes_prime_membership_price_bank_holiday_sale_thumb800_1.jpg?itok=stXBAC67" 
+          alt="" 
+          className="home__image" 
+        />
+
+        <div className="home__row">
+          <Product />
+        </div>
+
+        <div className="home__row">
+          
+        </div>
+
+        <div className="home__row">
+          
+        </div>
+      </div>
     </div>
   )
 }
